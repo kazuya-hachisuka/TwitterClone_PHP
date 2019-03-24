6177ce60a9c2643d878952ceb092abd80a638c52
kazuya-hachisuka <35261436+kazuya-hachisuka@users.noreply.github.com>
2019-03-24 16:04:49 +0900

    入力された情報を表示する

--- post/join/check.php
+++ post/join/check.php
@@ -28,9 +28,11 @@ if (!isset($_SESSION['join'])) {
 		<dl>
 		<dt>ニックネーム</dt>
 		<dd>
+      <?php echo htmlspecialchars($_SESSION['join']['name'], ENT_QUOTES); ?>
     </dd>
 		<dt>メールアドレス</dt>
 		<dd>
+      <?php echo htmlspecialchars($_SESSION['join']['email'], ENT_QUOTES); ?>
     </dd>
 		<dt>パスワード</dt>
 		<dd>
@@ -38,6 +40,7 @@ if (!isset($_SESSION['join'])) {
     </dd>
 		<dt>写真など</dt>
 		<dd>
+      <img src="../member_picture/<?php echo htmlspecialchars($_SESSION['join']['image'], ENT_QUOTES); ?> " width="100" height="100" alt="" />
     </dd>
 		</dl>
 		<div><a href="index.php?action=rewrite">&laquo;&nbsp;書き直す</a> | <imput type="submit" value="登録する" /></div>
