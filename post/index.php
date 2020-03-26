e3137002f2af36944a8f709adb016843bb3de843
kzy8sk <kzy8sk@gmail.com>
2020-03-26 21:44:52 +0900

    投稿画面作成

---
+++ post/index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<title>ひとこと掲示板</title>
+	<link rel="stylesheet" href="../style.css" />
+</head>
+<body>
+<div id="wrap">
+  <div id="head">
+    <h1>ひとこと掲示板</h1>
+  </div>
+  <div id="content">
+    <form action="" method="post">
+      <dl>
+        <dt>メッセージをどうぞ</dt>
+        <dd>
+          <textarea name="message" cols="50" rows="5"></textarea>
+        </dd>
+      </dl>
+      <div>
+        <input type="submit" value="投稿する"/>
+      </div>
+    </form>
+  </div>
+</div>
+</body>
+</html>
